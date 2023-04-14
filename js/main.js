6c480149933798a0e5a924e8b0323055f1136f17
UnrealPuggy <126524020+UnrealPuggy@users.noreply.github.com>
2023-04-13 18:07:52 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,37 +1,3 @@
-var datedisplay = 'none';
-		document.getElementById('date').style.display = datedisplay;
-        
-		function getTime() {
-				var today = new Date();
-				var date = (today.getMonth()+1)+'/'+today.getDate()+'/'+today.getFullYear();
-				var hours = today.getHours();
-				if(hours > 12) {
-					hours -= 12;	
-				}
-				var minutes = today.getMinutes();
-				if (minutes < 10) {
-					minutes = `0${today.getMinutes()}`;	
-				}
-				var seconds = today.getSeconds();
-				if (seconds < 10) {
-					seconds = `0${today.getSeconds()}`;	
-				}
-				var milli = today.getMilliseconds();
-				if (milli < 10) {
-					milli = `00${today.getMilliseconds()}`;	
-				} else if(milli < 100) {
-					milli = `0${today.getMilliseconds()}`;	
-				}
-				var time = hours + ":" + minutes + ":" + seconds+ ":" + milli;
-				var dateTime = date+' '+time;
-				
-					document.getElementById('date').style.display = datedisplay;
-				document.getElementById('date').innerHTML = dateTime;
-				today.remove();
-		}
-        function getId(id) {
-            return document.getElementById(id);
-        }
 //hover
 
 for(let i = 0; i < document.getElementsByTagName('button').length; i++) {
@@ -64,3 +30,36 @@ function clickSound() {
         sound.play()
         sound.remove()
 }
+var datedisplay = 'none';
+		document.getElementById('date').style.display = datedisplay;
+        
+		function getTime() {
+				var today = new Date();
+				var date = (today.getMonth()+1)+'/'+today.getDate()+'/'+today.getFullYear();
+				var hours = today.getHours();
+				if(hours > 12) {
+					hours -= 12;	
+				}
+				var minutes = today.getMinutes();
+				if (minutes < 10) {
+					minutes = `0${today.getMinutes()}`;	
+				}
+				var seconds = today.getSeconds();
+				if (seconds < 10) {
+					seconds = `0${today.getSeconds()}`;	
+				}
+				var milli = today.getMilliseconds();
+				if (milli < 10) {
+					milli = `00${today.getMilliseconds()}`;	
+				} else if(milli < 100) {
+					milli = `0${today.getMilliseconds()}`;	
+				}
+				var time = hours + ":" + minutes + ":" + seconds+ ":" + milli;
+				var dateTime = date+' '+time;
+				
+					document.getElementById('date').style.display = datedisplay;
+				document.getElementById('date').innerHTML = dateTime;
+		}
+        function getId(id) {
+            return document.getElementById(id);
+        }
